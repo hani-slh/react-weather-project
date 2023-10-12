5d3bc5fe0cf24f83f25a8062a979a0a2e43bb8cc
hani-slh <140309588+hani-slh@users.noreply.github.com>
2023-10-12 12:55:02 +0330

    addadded html and css

---
+++ src/Weather.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="App">
+      <div className="weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city"
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input type="submit" vlaue="search" className="btn btn-primary" />
+            </div>
+          </div>
+        </form>
+        <h1>New York </h1>
+        <ul>
+          <li>Thursday 07:00</li>
+          <li>Mostly cloudy</li>
+        </ul>
+        <div className="row mt-3">
+          <div className="col-6">
+            <div className="clearfix">
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                className="float-left"
+              />
+              <span className="temp">6</span> <span className="unit">°C</span>
+            </div>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Precipitation: 15%</li>
+              <li>Humidity: 70%</li>
+              <li>Wind: 14 km/h</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
